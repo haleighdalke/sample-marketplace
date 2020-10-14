186c0c54dfbbc85fce76b144a4e20cf795867ba3
haleighdalke <haleighdalke@gmail.com>
2020-10-14 16:44:47 -0400

    initial setup with react router

---
+++ src/containers/Landing.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useHistory } from 'react-router-dom'
+
+
+const LandingPage = () => {
+    let history = useHistory()
+
+    return (
+        <div className="landing">
+        </div>
+    )
+}
+
+export default LandingPage
